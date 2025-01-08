1807832083fc0158d8ca3d4cb965305828bf4af7
NakartSaW <ggl.clvr@gmail.com>
2025-01-08 15:05:13 +0100

    SafeCalculatorTest with Mockito

--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -1,8 +1,18 @@
 package info.dmerej;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class SafeCalculatorTest {
+
+  @Mock
+  private Authorizer authorizer;
+
   @Test
   void should_not_throw_when_authorized() {
 
@@ -14,6 +24,14 @@ public class SafeCalculatorTest {
     };
 
     SafeCalculator safeCalculator = new SafeCalculator(authorizer);
+    safeCalculator.add(1,1);
+  }
+
+  @Test
+  void should_not_throw_when_authorized_with_mockito() {
+    when(authorizer.authorize()).thenReturn(true);
+    SafeCalculator safeCalculator = new SafeCalculator(authorizer);
+
     safeCalculator.add(1,1);
   }
 }
