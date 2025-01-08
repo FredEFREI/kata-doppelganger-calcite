8a4a5d797dd52ebf688633c322a0231bc79b9e1c
FredEFREI <frederic.casier@efrei.net>
2025-01-08 15:34:08 +0100

    Test 2 implemented

--- java/src/test/java/info/dmerej/DiscountApplierTest.java
+++ java/src/test/java/info/dmerej/DiscountApplierTest.java
@@ -2,15 +2,47 @@ package info.dmerej;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscountApplierTest {
+  int discount = 10;
+  List<User> users = new ArrayList<User>();
+  List<User> test = new ArrayList<User>();
+
+  Notifier notifier = new Notifier() {
+    @Override
+    public void notify(User user, String message) {
+      test.add(user);
+      System.out.println(user.email() + " : " + message);
+    }
+  };
+
   @Test
   void should_notify_twice_when_applying_discount_for_two_users_v1() {
-    // TODO: write a test to demonstrate the bug in DiscountApplier.applyV1()
+    test.clear();
+
+    users.add(new User("test", "test.test@test.ts"));
+    users.add(new User("fred", "fred.cas@test.ts"));
+    users.add(new User("john", "john.vla@test.ts"));
+
+    DiscountApplier discountApplier = new DiscountApplier(notifier);
+    discountApplier.applyV1(discount, users);
+    assert test.size() == users.size();
   }
 
   @Test
   void should_notify_twice_when_applying_discount_for_two_users_v2() {
-    // TODO: write a test to demonstrate the bug in DiscountApplier.applyV2()
-  }
+    test.clear();
 
+    users.add(new User("test", "test.test@test.ts"));
+    users.add(new User("fred", "fred.cas@test.ts"));
+    users.add(new User("john", "john.vla@test.ts"));
+
+    DiscountApplier discountApplier = new DiscountApplier(notifier);
+    discountApplier.applyV2(discount, users);
+
+    assert test.equals(users);
+  }
 }
