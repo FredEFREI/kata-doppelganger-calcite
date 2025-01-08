cd3ab2ed18989ce2761fa967c68a92c89eee5945
FredEFREI <frederic.casier@efrei.net>
2025-01-08 14:53:37 +0100

    Test 1 implemented

--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -5,6 +5,15 @@ import org.junit.jupiter.api.Test;
 public class SafeCalculatorTest {
   @Test
   void should_not_throw_when_authorized() {
-    // TODO: write a test to demonstrate the bug in SafeCalculator.add()
+
+    Authorizer authorizer = new Authorizer() {
+      @Override
+      public boolean authorize() {
+        return true;
+      }
+    };
+
+    SafeCalculator safeCalculator = new SafeCalculator(authorizer);
+    safeCalculator.add(1,1);
   }
 }
